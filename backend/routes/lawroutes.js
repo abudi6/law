440432dc33e1ae850ffa0f8e42022aa22f18d274
abudi6 <aazzouni@ymail.com>
2023-08-15 10:37:23 +0800

    removed everything

--- backend/routes/lawroutes.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import {find as findHandler} from '../controllers/lawcon.js';
-
-const router = express.Router();
-
-router.post('/find', findHandler);
-
-export default router;

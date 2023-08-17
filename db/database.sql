6bc5234812ee7c5cc746166e5d561ff0fa4c7cfc
HeilKomisama <bowser0619@gmail.com>
2023-08-17 13:29:55 +0800

    added 'admin' column

--- db/database.sql
+++ db/database.sql
@@ -2,11 +2,12 @@ CREATE TABLE users (
     id INT AUTO_INCREMENT PRIMARY KEY,
     email VARCHAR(255) NOT NULL,
     username VARCHAR(255) NOT NULL,
-    password VARCHAR(255) NOT NULL
+    password VARCHAR(255) NOT NULL,
+    admin BOOLEAN NOT NULL
 );  
 
-INSERT INTO `users` (`id`, `email`, `username`, `password`) VALUES
-(1,'email@email.com','username', 'password');
+INSERT INTO `users` (`id`, `email`, `username`, `password`, 'admin') VALUES
+(1,'email@email.com','username', 'password', 'admin');
 
 CREATE TABLE laws (
     id INT AUTO_INCREMENT PRIMARY KEY,

ea23000d761e1eee31b499db6cc0b93e52a06454
abudi6 <aazzouni@ymail.com>
2023-08-25 20:02:13 +0800

    added a db for the bookmarks

--- db/database.sql
+++ db/database.sql
@@ -27,6 +27,38 @@ CREATE TABLE IF NOT EXISTS annotations (
     FOREIGN KEY (user_id) REFERENCES users(id)
 );
 
-CREATE TABLE annotations (
+CREATE TABLE IF NOT EXISTS users (
     id INT AUTO_INCREMENT PRIMARY KEY,
-)
+    email VARCHAR(255) NOT NULL,
+    username VARCHAR(255) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
+    admin BOOLEAN NOT NULL
+);
+
+INSERT INTO users (email, username, password, admin) VALUES
+('email@email.com', 'username', 'password', TRUE);
+
+
+CREATE TABLE laws (
+    lawId INT AUTO_INCREMENT PRIMARY KEY,
+    lawTitle VARCHAR(255) NOT NULL,
+    lawDescription TEXT NOT NULL,
+    type VARCHAR(50),
+    keywords VARCHAR(100)
+);
+
+
+CREATE TABLE IF NOT EXISTS annotations (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    annotationtext VARCHAR(255) NOT NULL,
+    user_id INT NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users(id)
+);
+
+CREATE TABLE IF NOT EXISTS bookmarks (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    law_id INT NOT NULL,
+    user_id INT NOT NULL,
+    FOREIGN KEY (law_id) REFERENCES laws(lawId),
+    FOREIGN KEY (user_id) REFERENCES users(id)
+);

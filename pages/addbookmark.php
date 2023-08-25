7e8af618100fbfc91691464ac7a8ad8abdbe53f1
abudi6 <aazzouni@ymail.com>
2023-08-25 19:31:15 +0800

    response to idk

--- pages/addbookmark.php
+++ pages/addbookmark.php
@@ -1,4 +1,8 @@
 <?php
 session_start();
 //actually not sure how to implement this
+// probably a new db that stores each law id corresponding to the user id
+// hmm when I think about it we could implement a button for that for each law
+// rather than making a new page for that
+
 ?>
